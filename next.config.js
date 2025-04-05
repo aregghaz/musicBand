868440d361b1037cc770ef5d8c6ac7ea285243d0
Areg <aregghaz@gmail.com>
2025-04-05 10:01:36 +0400

    as

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
 export default {
-  reactStrictMode: true, 
+  output: 'export',
   images: {
-    domains: ['picsum.photos','via.placeholder.com'],  // Add your external image domain here
+    unoptimized: true
   },
+
+  webpack: (config) => {
+    config.externals = [...config.externals, { canvas: "canvas" }];  // required to make Konva & react-konva work
+
+    // load worker files as a urls with `file-loader`
+    config.module.rules.unshift({
+      test: /pdf\.worker\.(min\.)?js/,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            name: "[contenthash].[ext]",
+            publicPath: ".next/static/worker",
+            outputPath: "static/worker"
+          }
+        }
+      ]
+    });
+
+    return config;
+  }
 };
