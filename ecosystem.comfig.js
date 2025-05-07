7791055d4b358a6eec93b64de59501fa7483c822
MayMartirosyan <mayis11.98@gmail.com>
2025-05-07 16:47:07 +0400

    chore: reset port

--- ecosystem.comfig.js
+++ ecosystem.comfig.js
@@ -6,8 +6,8 @@ module.exports = {
         args: 'run start',
         env: {
           NODE_ENV: 'production',
-          PORT: 3050,
-          BASE_URL: 'http://localhost:3050',
+          PORT: 3000,
+          BASE_URL: 'http://localhost:3000',
         },
       },
     ],
