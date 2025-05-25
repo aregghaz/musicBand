3e17f07ef0f7ad7c05d60a25d56489101afb6d21
MayMartirosyan <mayis11.98@gmail.com>
2025-05-25 14:10:06 +0400

    fix: favicon and button color

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,23 +3,35 @@ import './styles/globals.scss';
 import Footer from '@components/Common/Footer/Footer';
 import ScrollUp from '@uikit/ScrollUp/ScrollUp';
 import Nav from '@components/Common/Nav/Nav';
-import { BASE_URL, navigationItems } from '@utils/index';
+import { BASE_URL, STORAGE_URL, navigationItems } from '@utils/index';
 
 async function getData() {
   const res = await fetch(`${BASE_URL}/home-sections-manage`, {
     cache: 'no-store',
   });
-  return res.json();
+
+  const permissions = await res.json();
+
+  const fetchFavicon = await fetch(`${BASE_URL}/favicon`, {
+    cache: 'no-store',
+  });
+
+  const favicon = await fetchFavicon.json();
+
+  return [permissions, favicon];
 }
 
 export default async function Layout({ children }: { children: ReactNode }) {
-  const permissions = await getData();
+  const [permissions, favicon] = await getData();
+
+  console.log(favicon, 'favicon');
+
   return (
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="UTF-8" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href={`${STORAGE_URL}${favicon.faviconLink}`} />
       </head>
       <body>
         <Nav navItems={navigationItems} permissions={permissions.data} />
